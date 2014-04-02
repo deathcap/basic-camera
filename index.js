2720175e861aef03c4c494949fddbf499dfabd1e
deathcap <deathcap@gmx.co.uk>
2014-04-01 19:27:32 -0700

    Extract camera vector from rotation matrix

--- index.js
+++ index.js
@@ -10,7 +10,20 @@ function noclip(position) {
 
   this.position = position || vec3.create()
   this.rotationX = this.rotationY = this.rotationZ = 0.0
+  this.cameraVector = vec3.create()
 }
+
+noclip.prototype.updateCameraVector = function(v, m) {
+  // extract the direction vector (v) from 3x3 rotation subset of matrix (m)
+  // see THREE.js Vector3 transformDirection, and voxel-view cameraVector()
+
+  v[0] = m[0] + m[4] + m[8]
+  v[1] = m[1] + m[5] + m[9]
+  v[2] = m[2] + m[6] + m[10]
+
+  vec3.normalize(v, v)
+}
+
 
 noclip.prototype.view = function(output) {
   if (!output) output = mat4.create()
@@ -18,6 +31,9 @@ noclip.prototype.view = function(output) {
   mat4.rotateX(output, output, this.rotationX)
   mat4.rotateY(output, output, this.rotationY)
   mat4.rotateZ(output, output, this.rotationZ)
+
+  this.updateCameraVector(this.cameraVector, output)
+
   mat4.translate(output
     , output
     , this.position
